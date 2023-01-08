5b75eb5bcfe0c4ea5500304af8691be1d5747d80
Rohit Tiwari <110346246+tiwaritech18@users.noreply.github.com>
2023-01-08 19:38:44 +0530

    initial stage!

---
+++ script.js
@@ -0,0 +1,21 @@
+let questions=[{id:0,
+q:"Javacript is an _______ language?",
+a:[{text:"Object-Oriented",isCorrect:true},
+{text:"Object-Based",isCorrect:false },
+{text:"procedural",isCorrect:false},
+{text:"None of the Above"}] 
+},
+{id:1,
+q:"which of the following keywords is used to define a varible in jaascript?",
+a:[{tecxt:"var",isCorrect: false},
+{text:"let",isCorrect:false},
+{text:"both A and B",isCorrect:true},
+{text:"None of the above",isCorrect:false}]}
+]
+
+//set start
+var start=true;
+//iterate
+function iterate(id){
+    var result
+}
